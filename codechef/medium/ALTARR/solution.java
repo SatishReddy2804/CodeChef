ff181ba53b9b6f0b9688e4e478d6927e4f0e4237
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-07-15 20:20:46 +0530

    ✅ ALTARR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ codechef/medium/ALTARR/solution.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int c=0;
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++);
+            {
+                arr[i]=sc.nextInt();
+            }
+            int x=-1;
+            int y=-1;
+            if(arr[0]%2==0)
+            {
+                x=0;
+            }
+            else
+            {
+                x=1;
+            }
+            for(int i=1;i<arr.length;i++)
+            {
+                if(arr[i]%2==0)
+                {
+                    y=0;
+                }
+                else
+                {
+                    y=1;
+                }
+                if(x==y)
+                {
+                    c++;
+                }
+                else
+                {
+                    x=y;
+                }
+            }
+            System.out.println(c);
+        }
+	}
+}
