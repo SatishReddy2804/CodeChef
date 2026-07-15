06c0a6e5cb847c40d2d29577db3841d874462d42
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-07-15 20:59:51 +0530

    ✅ CONVEYOR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/CONVEYOR/solution.java
+++ codechef/medium/CONVEYOR/solution.java
@@ -2,7 +2,12 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        import java.util.*;
 
 class Codechef {
     public static void main(String[] args) {
