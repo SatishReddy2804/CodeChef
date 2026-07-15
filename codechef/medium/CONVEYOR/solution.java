bf8e42052a7b4e38298cc9f12b3e29229cb8a3de
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-07-15 20:57:06 +0530

    ✅ CONVEYOR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/CONVEYOR/solution.java
+++ codechef/medium/CONVEYOR/solution.java
@@ -7,6 +7,20 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+        import java.util.*;
 
-	}
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int p=sc.nextInt();
+            String s=sc.next();
+            int left=0,right=0;
+            for(int i=0;i<p;i++) if(s.charAt(i)!='L') left++;
+            for(int i=p-1;i<n;i++) if(s.charAt(i)!='R') right++;
+            System.out.println(Math.min(left,right));
+        }
+    }
 }
