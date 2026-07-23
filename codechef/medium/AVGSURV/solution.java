b2cb93d586ccfbda5fe41337954df7c9b3bfdcc8
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-07-23 21:24:43 +0530

    ✅ AVGSURV | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/medium/AVGSURV/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            int max=Integer.MIN_VALUE;
+            for(int i=0;i<n;i++) {
+                arr[i]=sc.nextInt();
+                if(arr[i]>max) 
+                {
+                    max=arr[i];
+                }
+            }
+            int c=0;
+            for(int i=0;i<n;i++) 
+            {
+                if(arr[i]==max) 
+                {
+                    c++;
+                }
+            }
+            System.out.println(c);
+        }
+    }
+}
