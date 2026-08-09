a219526730ab19927a11257f2721e7c5f1dc0ec9
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-08-09 10:44:16 +0530

    ✅ CBSPEED - Rating 288 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-09

---
+++ codechef/easy/CBSPEED---Rating-288/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// write your code here
+		System.out.println((y>x)?"YES":"NO");
+		
+	}
+}
