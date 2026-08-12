09ec57ee59b785ab42de7cbef180487589379992
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-08-12 20:56:03 +0530

    ✅ BUSROW | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/BUSROW/solution.java
+++ codechef/medium/BUSROW/solution.java
@@ -14,8 +14,9 @@ class Codechef
             int n=sc.nextInt();
             int m=sc.nextInt();
             int x=sc.nextInt();
-            int r=(x-1)/m+1;
-            System.out.println(Math.min(r,n-r+1));
+            int s=(x-1)%m+1;
+            int res=Math.min(s,m-s+1);
+            System.out.println(res);
         }
 	}
 }
