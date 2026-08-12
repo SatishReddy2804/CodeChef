290f562c0071dc23f592d01fcdce9e67a44defca
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-08-12 20:27:25 +0530

    ✅ CHKEV | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

---
+++ codechef/medium/CHKEV/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextint();
+        int falg=0;
+        for(int i=a;i<=b;i++)
+        {
+            if(i%2==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        System.out.println((flag==1)>"YES":"NO");
+	}
+}
