e05199de1610d4793bc27f502d83d7a6cd9e1ac1
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-08-12 20:37:44 +0530

    ✅ BUSROW | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

---
+++ codechef/medium/BUSROW/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int x=sc.nextInt();
+            int z=x/m;
+            if(n==1)
+            {
+                System.out.println("1");
+            }
+            else
+            {
+                System.out.println(Math.abs(Math.min(z-n,m-z)));
+            }
+        }
+	}
+}
