7eb85bd110c8e2530c696443feeaf21fc5c1222d
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-08-30 11:06:21 +0530

    ✅ MORNINGRUN - Rating 306 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-30

---
+++ codechef/easy/MORNINGRUN---Rating-306/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println((2*(l+b)>=1000)?"YES":"NO");
+	}
+}
