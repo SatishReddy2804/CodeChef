ec0b53b1c9808409ff73edb18a13e4dacf4fd6fd
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-09-01 07:36:14 +0530

    ✅ TCG - Rating 311 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-01

--- codechef/easy/TCG---Rating-311/solution.java
+++ codechef/easy/TCG---Rating-311/solution.java
@@ -10,6 +10,7 @@ class Codechef
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
         int y=sc.nextInt();
+        if(x==y) System.out.println("SAME");
         System.out.println((x>y)?"DECREASED":"INCREASED");
 	}
 }
