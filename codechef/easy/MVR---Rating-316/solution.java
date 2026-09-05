4576dafe297bd053b2127c0fd61e9c6da4d32ecf
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-09-05 19:29:31 +0530

    ✅ MVR - Rating 316 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/easy/MVR---Rating-316/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(a*2+b<x*2+y)
+        {
+            System.out.println("Ronaldo");
+        }
+        else if(a*2+b==x*2+y)
+        {
+            System.out.println("Equal");
+        }
+        else
+        {
+            System.out.println("Messi");
+        }
+	}
+}
