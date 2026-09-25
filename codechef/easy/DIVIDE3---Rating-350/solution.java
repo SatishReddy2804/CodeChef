9bd6a7df3b1b9d7d4ed35ac553460d2e74534371
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2026-09-25 08:03:33 +0530

    ✅ DIVIDE3 - Rating 350 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-25

--- codechef/easy/DIVIDE3---Rating-350/solution.java
+++ codechef/easy/DIVIDE3---Rating-350/solution.java
@@ -9,17 +9,6 @@ class Codechef
 		// your code goes here
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
-        if(x%3==0)
-        {
-            System.out.println("0");
-	    }
-	    else if(x%3==1)
-	    {
-	        System.out.println("2");
-	    }
-	    else
-	    {
-	        System.out.println("1");
-	    }
+        System.out.println(Math.min(x%3,3-(x%3)));
 	}
 }
